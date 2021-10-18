cac0fc76a74e1de70bc882760747ac275ac667ad
David <davidallenr@gmail.com>
2021-10-18 10:16:54 -0400

    Designing Program

--- consumer.cpp
+++ consumer.cpp
@@ -3,5 +3,20 @@
 
 
 int main () {
+    //Create variable
+    //Create semaphores
     
+    // Make shared process between prod and cons
+    // Set the size of the mem space
+
+    // Loop through consumer
+        // wait (fill)
+        // wait on mutex
+        // decrease size
+        // post mutex
+        // post (available)
+
+    // Close/unlink connections
+    // Remove shared memory
+
 }
--- producer.cpp
+++ producer.cpp
@@ -3,5 +3,19 @@
 
 
 int main () {
+    //Create variable
+    //Create semaphores
     
+    // Make shared process between prod and cons
+    // Set the size of the mem space
+
+    // Loop through production
+        // wait (available)
+        // wait on mutex
+        // increase size
+        // post mutex
+        // post (fill)
+
+    // Close/unlink connections
+    // Remove shared memory
 }
